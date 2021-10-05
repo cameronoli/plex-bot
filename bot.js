f1b151a6f1a16f6823fc0905a7bacb3bbb4f2e61
Cameron Oliver <cameron.olivernz@hotmail.com>
2021-10-05 15:07:54 +1300

    decided to use python in place of node

--- bot.js
+++ bot.js
@@ -1,19 +1,23 @@
 const { Client, Intents } = require('discord.js');
 const dcord = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const cmd_prefix = '$';
-//const qbit_api = require('qbittorrent-api');
-//const qbt = qbit_api.connect('<can't use contnainer name in swarm>:14715');
-const torrent_error = 'Please use a magent link or a URL to a torrent file. See "$help" for more information.';
-const help_file = 'This is a bot for adding movies or T.V shows to the Plex media server "Beauty"\
-Bot Commands\
-- $ping; Returns "Pong!"\
-- $dl, $download {type} {link}\
-    - {type}; Use "movie" when downloading a movie or "tv" when downloading a T.V show\
-    - {link}; You must use either a magnet link or a URL to a torrent file\
-        E.G. - Magnet: magnet:?xt=urn:btih:f5caf1161cbe9312511cf1c35a75c744e7eb8e67&dn=Survivor.S41E02.1080p.AMZN.WEBRip.DDP5.1.x264-KiNGS%5Brartv%5D&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2950&tr=udp%3A%2F%2F9.rarbg.to%3A2940&tr=udp%3A%2F%2Ftracker.fatkhoala.org%3A13750&tr=udp%3A%2F%2Ftracker.slowcheetah.org%3A14800\
-             - URL: https://rarbg.to/download.php?id=mhy6u3a&h=f5c&f=Survivor.S41E02.1080p.AMZN.WEBRip.DDP5.1.x264-KiNGS%5Brartv%5D-[rarbg.to].torrent\
-             ';
 
+const qbittorrentapi = require('qbittorrent-api-v2');
+const qbt_client = qbittorrentapi.Client(host='torrent:8080', username='admin', password='adminadmin');
+//const qbt_api = require('qbittorrent-api-v2');
+//const qbt = qbt_api.connect('test_torrent:8080', 'admin', 'adminadmin');
+console.log(qbt_client.app.version);
+
+const torrent_error = 'Please use a magent link or a URL to a torrent file. See "$help" for more information.';
+const help_file = 'This is a bot for adding movies or T.V shows to the Plex media server "Beauty" \n \
+Bot Commands \n \
+- $ping; Returns "Pong!" \n \
+- $dl, $download {type} {link} \n \
+    - {type}; Use "movie" when downloading a movie or "tv" when downloading a T.V show \n \
+    - {link}; You must use either a magnet link or a Url to a torrent file \n \
+        E.G. - Magnet: magnet:?xt=urn:btih:f5caf1161cbe9312511cf1c35a75c744e7eb8e67&dn=Survivor.S41E02.1080p%5D \n \
+             - Url: https://rarbg.to/download.php?id=mhy6u3a&h=f5c&f=Survivor.S41E02.1080p.AMZN.WEBRip.DDP5.1.x264-KiNGS%5Brartv%5D-[rarbg.to].torrent \n \
+             '
 dcord.once('ready', () =>{
     console.log('PlexBot is online!');
 });
@@ -29,11 +33,11 @@ dcord.on('message', messageCreate =>{
     }
     if(command === 'download' || command === 'dl'){
         messageCreate.channel.send('Downloading ' + String(args[0]) + ' with ' + String(args[1]));
-        //qbt.add(args[1], <save_path (volume - media?)>, args[0], function (error){messageCreate.channel.send(torrent_error)})
+        qbt.add(args[1], "/downloads", args[0], function (error){messageCreate.channel.send(torrent_error)});
     }
     if(command === 'help' || command === 'h'){
         messageCreate.channel.send(help_file)
     }
 });
 
-dcord.login('<enter bot token here>');
+dcord.login('token');
