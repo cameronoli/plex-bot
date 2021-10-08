b79fe8a137971a4cea0ca8d39932e23049de9a02
Cameron Oliver <cameron.olivernz@hotmail.com>
2021-10-08 15:28:01 +1300

    updating images

--- devbot.py
+++ devbot.py
@@ -13,18 +13,18 @@ torrent_added = "Wohoo! You've just started the download for your file. Check Pl
 invalid_cat = "Is not a valid category. Please use 'movie' or 'tv'."
 invalid_link = "The URL/magnet you provided is not valid. Please find another."
 
-bot = commands.Bot(command_prefix='$', help_command=None)
+bot = commands.Bot(command_prefix='!', help_command=None)
 help_file = '```This is a bot for adding movies or T.V shows to the Plex media server "Beauty" \n \
 \n \
 Bot Commands \n \
-- $h, $help; Returns this message    \n \
-- $ping; Returns "Pong!" \n \
-- $dl, $download type link \n \
+- !h, $help; Returns this message    \n \
+- !ping; Returns "Pong!" \n \
+- !dl, $download type link \n \
     - [type]; Use "movie" when downloading a movie or "tv" when downloading a T.V show \n \
     - [link]; You must use either a magnet link or a Url to a torrent file \n \
         E.G. - Magnet: magnet:?xt=urn:btih:f5caf1161cbe9312511cf1c35a75c744e7eb8e67&dn=Survivor.S41E02.1080p%5D \n \
              - Url: https://rarbg.to/download.php?id=mhy6u3a&h=f5c&f=Survivor.S41E02.1080p.AMZN.WEBRip.DDP5.1.x264-KiNGS%5Brartv%5D-[rarbg.to].torrent \n \
-- $v; Returns the installed version of qbittorrent API\n \
+- !v; Returns the installed version of qbittorrent API\n \
 ```'
 
 @bot.command()
