0deff305c99cf81732e41deec2fff97c2693c93b
Cameron Oliver <cameron.olivernz@hotmail.com>
2021-11-03 21:16:27 +1300

    updated

--- devbot.py
+++ devbot.py
@@ -17,28 +17,33 @@ print("Logged in.")
 #------------------------------------------------------------------------------------------------------
 
 version = qbt_client.app.version
-torrent_added = "Wohoo! You've just started the download for your file. Check Plex shortly to watch!"
+torrent_added = "Wohoo! You've just started the download for your file. use !info to check the status of in-progress torrents."
 invalid_cat = " is not a valid category. Please use 'movie' or 'tv'."
 invalid_link = "The URL/magnet you provided is not valid. Please find another."
 
-bot = commands.Bot(command_prefix='!', help_command=None)
+bot = commands.Bot(command_prefix='$', help_command=None) #make sure to set this to the correct symbol
 help_file = '```This is a bot for adding movies or T.V shows to the Plex media server "Beauty" \n \
 \n \
 Bot Commands \n \
-- !h, !help; Returns this message    \n \
-- !ping; Returns "Pong!" \n \
-- !dl, $download type link \n \
+- $address; Returns the URL of the Plex server \n \
+- $dl, $download type link \n \
     - [type]; Use "movie" when downloading a movie or "tv" when downloading a T.V show \n \
     - [link]; You must use either a magnet link or a Url to a torrent file \n \
         E.G. - Magnet: magnet:?xt=urn:btih:f5caf1161cbe9312511cf1c35a75c744e7eb8e67&dn=Survivor.S41E02.1080p%5D \n \
              - Url: https://rarbg.to/download.php?id=mhy6u3a&h=f5c&f=Survivor.S41E02.1080p.AMZN.WEBRip.DDP5.1.x264-KiNGS%5Brartv%5D-[rarbg.to].torrent \n \
-- !v; Returns the installed version of qbittorrent API \n \
-- !info; Returns info on all in-progress torrents \n \
+- $h, $help; Returns this message \n \
+- $info; Returns info on all in-progress torrents \n \
+- $ping; Returns "Pong!" \n \
+- $v; Returns the installed version of qbittorrent API \n \
 ```'
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@bot.command()
+async def address(ctx):
+    await ctx.send('```http://watch.oliverlegacy.net```')
 
 @bot.command()
 async def help(ctx):
